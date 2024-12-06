aee3f8c459c4cb2628f99276993d57a4caa5804b
Андрей Степанов <therain3164@yandex.ru>
2024-12-06 20:30:32 +0300

    Initial Commit

---
+++ KeepLink/KeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepLinkApp.swift
+//  KeepLink
+//
+//  Created by Андрей Степанов on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct KeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
