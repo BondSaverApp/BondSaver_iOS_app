5ecc800e9c48fda392c77d42b733b1a0a7f8bcce
Андрей Степанов <therain3164@yandex.ru>
2025-02-17 11:55:55 +0300

    migration scheme 3

--- KeepLink/KeepLinkApp.swift
+++ KeepLink/KeepLinkApp.swift
@@ -9,11 +9,16 @@ import SwiftUI
 import RealmSwift
 
 let config = Realm.Configuration(
-    schemaVersion: 2,
+    schemaVersion: 3,
     migrationBlock: { migration, oldSchemaVersion in
         if oldSchemaVersion < 2 {
             migration.enumerateObjects(ofType: Contact.className()) { oldObject, newObject in
                 newObject!["avatarData"] = nil
+            }
+        }
+        if oldSchemaVersion < 3 {
+            migration.enumerateObjects(ofType: User.className()) { oldObject, newObject in
+                newObject!["phoneNumber"] = ""
             }
         }
     }
