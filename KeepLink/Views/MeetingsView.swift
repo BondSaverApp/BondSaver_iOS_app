cb3869e4bc179a205ef0d105ebaf95ecfbcb7529
Андрей Степанов <therain3164@yandex.ru>
2025-03-08 18:55:11 +0300

    adding fix

---
+++ KeepLink/Views/MeetingsView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingsView.swift
+//  KeepLink
+//
+//  Created by Андрей Степанов on 08.03.2025.
+//
+
+import SwiftUI
+
+struct MeetingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingsView()
+}
